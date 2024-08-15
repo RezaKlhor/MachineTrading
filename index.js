4e2e5ab57cbd550598e949d3dfa15686ea974695
= <reza.kalhor19@gmail.com>
2024-08-15 18:04:08 +0330

    fix: all unused apis removed

--- index.js
+++ index.js
@@ -113,11 +113,6 @@ try {
   app.get("/api/ath/getAll", athResolver.getAll);
   app.get("/api/ath/getOne", athResolver.getOne);
   app.get("/api/predict/getAvailablePredicts", aiResolver.getAvailablePredicts);
-  app.get("/api/predict/stockPredict", aiResolver.getStockPrediction);
-  app.get("/api/predict/weeklyPredict", aiResolver.getAllWeeklyPrediction);
-  app.get("/api/predict/monthlyPredict", aiResolver.getAllMonthlyPrediction);
-  app.get("/api/predict/oscPredict", aiResolver.getOscPrediction);
-  app.get("/api/predict/overall", aiResolver.getOverall);
   app.get(
     "/api/predict/weeklyPredictObserve",
     aiResolver.getAllWeeklyPredictionObserve
